966996ac6e479f7569be5ce93ad68071d0fc8a68
SpiffyEight77 <killer_0248@live.com>
2021-09-23 19:12:59 +0800

    docs: Update repository

--- GameCard/ContentView.swift
+++ GameCard/ContentView.swift
@@ -9,8 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+//            Text(verbatim:"Platform")
+//                            .font(.largeTitle)
+//                            .fontWeight(.heavy)
+//                            .foregroundColor(.primary)
+//            Image("Cover")
+//                .resizable()
+//                .aspectRatio(contentMode: .fit)
+//
+//            Image("steam")
+//                .resizable()
+//                .aspectRatio(contentMode: .fit)
+            ZStack {
+                TabView {
+                            Text("Game View")
+                                .padding()
+                                .tabItem {
+                                    Image(systemName: "gamecontroller.fill")
+                                    Text("Consoles")
+                                }
+                                .tag(1)
+                            Text("Liked View")
+                                .padding()
+                                .tabItem {
+                                    Image(systemName: "bookmark.fill")
+                                    Text("Save")
+                                }
+                                .tag(2)
+                            Text("Game Play Status View")
+                                .padding()
+                                .tabItem {
+                                    Image(systemName: "dpad.fill")
+                                    Text("Status")
+                                }
+                                .tag(3)
+                            Text("Search View")
+                                .padding()
+                                .tabItem {
+                                    Image(systemName: "magnifyingglass")
+                                    Text("Search")
+                                }
+                                .tag(4)
+                            Text("Setting View")
+                                .padding()
+                                .tabItem {
+                                    Image(systemName: "person.fill")
+                                    Text("Profile")
+                                }
+                                .tag(5)
+                        }
+                HStack {
+                    
+                }
+            }
+        }
     }
 }
 
---
+++ GameCard/SettingView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingView.swift
+//  GameCard
+//
+//  Created by wayne on 2021/9/23.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        return Text("Mother Fucker!").padding()
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
