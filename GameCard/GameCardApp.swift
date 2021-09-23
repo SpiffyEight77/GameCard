5d78c318c4f127f8deff7f01310ef212d2b86409
SpiffyEight77 <killer_0248@live.com>
2021-09-23 12:02:56 +0800

    docs: Initial repository

---
+++ GameCard/GameCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameCardApp.swift
+//  GameCard
+//
+//  Created by wayne on 2021/9/23.
+//
+
+import SwiftUI
+
+@main
+struct GameCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
